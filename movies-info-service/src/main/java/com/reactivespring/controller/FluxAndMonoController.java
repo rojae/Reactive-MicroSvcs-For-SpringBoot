130ddd9730bcb0266e57697d27f5a8fbcc784700
rojae <rojae@kakao.com>
2022-10-10 04:44:11 +0900

    add. Flux junit testcase 3-way approach

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -1,9 +1,12 @@
 package com.reactivespring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class FluxAndMonoController {
@@ -17,6 +20,12 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     public Mono<String> mono(){
         return Mono.just("hello-world")
+                .log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream(){
+        return Flux.interval(Duration.ofSeconds(1))
                 .log();
     }
 
---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,64 @@
+package com.reactivespring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebFluxTest(controllers = FluxAndMonoController.class)
+@AutoConfigureWebTestClient
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void flux_approach1() {
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                //.expectBody().equals(List.of(1,2,3));
+                .expectBodyList(Integer.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void flux_approach2(){
+        var flux = webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext( 1,2,3)
+                .verifyComplete();
+    }
+
+
+    @Test
+    void flux_approach3 (){
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var responseBody = listEntityExchangeResult.getResponseBody();
+                    assert (Objects.requireNonNull(responseBody).size() == 3);
+                });
+    }
+
+}
