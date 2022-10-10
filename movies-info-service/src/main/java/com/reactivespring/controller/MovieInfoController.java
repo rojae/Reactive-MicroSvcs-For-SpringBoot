0d2883f75752c82f5f1636d6275f132af3b9983a
rojae <rojae@kakao.com>
2022-10-10 19:47:21 +0900

    add. Api adding movieInfo with testcase

--- movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
@@ -1,6 +1,7 @@
 package com.reactivespring.controller;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MoviesInfoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -9,5 +10,16 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1")
 public class MovieInfoController {
 
+    private MoviesInfoService moviesInfoService;
+
+    public MovieInfoController(MoviesInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo){
+        return moviesInfoService.addMovieInfo(movieInfo).log();
+    }
 
 }
---
+++ movies-info-service/src/test/java/intg/com/reactivespring/controller/MovieInfoControllerTest.java
@@ -0,0 +1,76 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.0.0")
+@AutoConfigureWebTestClient
+class MovieInfoControllerTest {
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    static String MOVIES_INFO_URL = "/v1/movieinfos";
+
+    @BeforeEach
+    void setup() {
+        var movieInfoList = List.of(new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+
+        movieInfoRepository.saveAll(movieInfoList)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void addMovieInfo() {
+        // given
+        var MovieInfo = new MovieInfo(null, "Batman Begins",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        // when
+        webTestClient.post()
+                .uri(MOVIES_INFO_URL)
+                .bodyValue(MovieInfo)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    var savedMovieInfo = movieInfoEntityExchangeResult.getResponseBody();
+                    assert savedMovieInfo != null;
+                    assert savedMovieInfo.getMovieInfoId() != null;
+                });
+
+        // then
+    }
+}
