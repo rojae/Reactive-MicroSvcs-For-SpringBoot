fa528a762b498a2f6f3bffd7122bc501023ca5e7
rojae <rojae@kakao.com>
2022-10-10 19:25:19 +0900

    add. MovieInfo Integrate testcase

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
@@ -0,0 +1,13 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+public class MovieInfoController {
+
+
+}
--- movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
+++ movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
@@ -13,6 +13,9 @@ import reactor.test.StepVerifier;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DataMongoTest
 @TestPropertySource(properties = "spring.mongodb.embedded.version=3.0.0")
 @ActiveProfiles("test")
@@ -35,7 +38,7 @@ class MovieInfoRepositoryIntgTest {
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         movieInfoRepository.deleteAll().block();
     }
 
@@ -51,4 +54,71 @@ class MovieInfoRepositoryIntgTest {
                 .expectNextCount(3)
                 .verifyComplete();
     }
+
+    @Test
+    void findById() {
+        // given
+
+        // when
+        var movieInfoMono = movieInfoRepository.findById("abc").log();
+
+        // then
+        StepVerifier.create(movieInfoMono)
+                .assertNext(movieInfo -> {
+                    assertEquals("Dark Knight Rises", movieInfo.getName());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void saveMovieInfo(){
+        // given
+        var newMovieInfo = new MovieInfo(null, "Batman Begins",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        // when
+        var moviesInfoMono = movieInfoRepository.save(newMovieInfo).log();
+
+        // then
+        StepVerifier.create(moviesInfoMono)
+                .assertNext(movieInfo -> {
+                    assertEquals(movieInfo.getName(), "Batman Begins");
+                    assertNotNull(movieInfo.getMovieInfoId());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void updateMovieInfo(){
+        // given
+        var movieInfo = movieInfoRepository.findById("abc").block();
+        movieInfo.setYear(2022);
+
+        // when
+        var movieInfoMono = movieInfoRepository.save(movieInfo).log();
+
+        // then
+        StepVerifier.create(movieInfoMono)
+                .assertNext(movieInfo1 -> {
+                    assertEquals(movieInfo1.getYear(), 2022);
+                    assertEquals(movieInfo1.getMovieInfoId(), "abc");
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void deleteMovieInfo(){
+        // given
+
+        // when
+        movieInfoRepository.deleteById("abc").log().block();
+        var moviesInfo = movieInfoRepository.findAll().log();
+
+        // then
+        StepVerifier.create(moviesInfo)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+
 }
