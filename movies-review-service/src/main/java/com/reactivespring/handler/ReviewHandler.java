8de846b961a3e4a68f107deee4c56b096bfc70ef
rojae <rojae@kakao.com>
2022-10-15 00:00:47 +0900

    Review Route

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,23 @@
 package com.reactivespring.handler;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
 public class ReviewHandler {
+
+    ReviewReactiveRepository reviewReactiveRepository;
+
+    public Mono<ServerResponse> addReview(ServerRequest request){
+        return request.bodyToMono(Review.class)
+                .flatMap(reviewReactiveRepository::save)
+                .flatMap(ServerResponse.status(HttpStatus.OK)::bodyValue);
+    }
+
+
 }
--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -1,5 +1,6 @@
 package com.reactivespring.router;
 
+import com.reactivespring.handler.ReviewHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -11,9 +12,10 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class ReviewRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> reviewsRoute() {
+    public RouterFunction<ServerResponse> reviewsRoute(ReviewHandler reviewHandler) {
         return route()
                 .GET("/v1/hello-world", (request -> ServerResponse.ok().bodyValue("hello-world")))
+                .POST("/v1/reviews", request -> reviewHandler.addReview(request))
                 .build();
     }
 }
